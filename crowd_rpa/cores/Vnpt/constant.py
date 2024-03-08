5ae9ae77cf451bca21e2958e5b5f9d0d64891245
tienloi123 <doantienloi123@gmail.com>
2024-03-09 04:26:41 +0700

    update function feat: wite core rpa for VNPT

--- crowd_rpa/cores/Vnpt/constant.py
+++ crowd_rpa/cores/Vnpt/constant.py
@@ -4,10 +4,21 @@ from settings import cfg
 
 
 class VnptConstant:
-    DOWNLOAD_BTN_BY_CLASS_TYPE = "//body/div[1]/a[1]"
-    DOWNLOAD_PDF_XPATH = '//*[@id="popup-content-container"]/div[1]/div[2]/div[12]/div/div/div'
-    DOWNLOAD_XML_XPATH = '//*[@id="popup-content-container"]/div[1]/div[2]/div[12]/div/div/div[2]'
-    URL = 'https://0312146685-tt78.vnpt-invoice.com.vn/'
+    FILE_PATH = './RPA_TEMP/VNPT.pdf'
+    # URL_KEYWORD = 'PortalLink'  # tag name contains data
+    URL_KEYWORD = 'Tra cứ u hóa đơn tại website:'
+    # CODE_BILL_KEYWORD = 'Fkey'
+    CODE_BILL_KEYWORD = 'Mã tra cứu:'
+    LAST_KEYWORD = 'Phí'
+
+    FORM_BY_ID_TYPE = 'Searchform'
+    ID_INPUT_BY_ID_TYPE = 'strFkey'
+    CAPTCHA_IMG_BY_CLASS_TYPE = 'captcha_img'
+    CAPTCHA_INPUT_BY_ID_TYPE = 'captch'
+    ERROR_ALERT_BY_XPATH = '//*[@id="messagewrapper"]/div'
+    VIEW_BTN_BY_XPATH = '//*[@class="table"]/tbody/tr/td[10]/a'
+    DOWNLOAD_BTN_BY_NAME_TYPE = 'down'
+    URL = None
     CORE_NAME = 'CORES.VNPT.RPA'
     META_DATA = {
         'URL': URL,
@@ -24,9 +35,8 @@ class VnptConstant:
     LATEST_VERSION = 'v1'
     DELAY_OPEN_MAXIMUM_BROWSER = 0.2
     DELAY_TIME_LOAD_PAGE = 3
-    DELAY_TIME_LOAD_CAPTCHA = 2
     DELAY_CLICK_DOWNLOAD_EVERY_FILE = 3
     DELAY_TIME_SKIP = 1
-
+    RETRY_MAX = 5
 
 vnpt_constant = VnptConstant()
--- crowd_rpa/cores/Vnpt/rpa.py
+++ crowd_rpa/cores/Vnpt/rpa.py
@@ -1,26 +1,36 @@
-import os
-import pathlib
 import time
 import logging
 from abc import ABC
 from driver import WebDriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import TimeoutException
 from crowd_rpa.interfaces.rpa_interface import IRpa
 from crowd_rpa.cores.Vnpt.constant import vnpt_constant
-import easyocr
-import cv2
-import re
-import numpy as np
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-
+from crowd_rpa.utils.rpa_util import util_rpa
 class VnptRpa(IRpa, ABC):
     def __init__(self, meta_data):
         super().__init__(meta_data)
+    def get_portal(self):
+        url = ""
+        path = vnpt_constant.FILE_PATH
+        if path.endswith(".pdf"):
+            url = util_rpa.read_pdf(path, vnpt_constant.URL_KEYWORD,
+                                    vnpt_constant.CODE_BILL_KEYWORD)
+        elif path.endswith(".xml"):
+            url = util_rpa.read_xml(path, vnpt_constant.URL_KEYWORD)
 
+        return url
+    def get_code_lookup(self):
+        code_bill = ""
+        path = vnpt_constant.FILE_PATH
+        if path.endswith(".pdf"):
+            code_bill = util_rpa.read_pdf(path, vnpt_constant.CODE_BILL_KEYWORD,
+                                          vnpt_constant.LAST_KEYWORD)
+        elif path.endswith(".xml"):
+            code_bill = util_rpa.read_xml(path, vnpt_constant.CODE_BILL_KEYWORD)
+        return code_bill
+
+    def check_invoice(self):
+        return None
     def extract_data(self):
         logging.info(f'{self.get_name()}: Start extracting data')
         self.process_download_xml_pdf()
@@ -30,108 +40,50 @@ class VnptRpa(IRpa, ABC):
 
     def get_name(self):
         return vnpt_constant.META_DATA['RPA_NAME']
-
-    def get_code_lookup(self):
-        return vnpt_constant.META_DATA['URL']
+    def enter_id(self,browser):
+        id_input = browser.find_element(By.NAME, "strFkey")
+        id_input.send_keys(self.get_code_lookup())
+        logging.info(f'{self.get_name()}: Enter id')
 
     def process_download_xml_pdf(self):
         logging.info(f'{self.get_name()}: Start process download xml & pdf')
-
-        # Function to handle captcha input
-        def handle_captcha_input(browser):
-            nhaplieu = browser.find_element(By.NAME, "strFkey")
-            nhaplieu.send_keys("009FA9BFCB2380469487DB4D7A54EE51FB")
-            time.sleep(vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
-            # Capture screenshot of the captcha
-            capcha = browser.find_element(By.NAME, "captch")
-            time.sleep(1)
-            browser.get_screenshot_as_file("captcha.png")
-            imgCv2 = cv2.imread('captcha.png')
-            # Apply image processing to enhance contrast and visibility
-            img_gray = cv2.cvtColor(imgCv2, cv2.COLOR_BGR2GRAY)
-            img_equ = cv2.equalizeHist(img_gray)
-
-            # Áp dụng kỹ thuật ngưỡng để chuyển đổi ảnh thành ảnh nhị phân, giúp làm nổi bật các đặc trưng của ảnh.
-            _, img_thresh = cv2.threshold(img_equ, 128, 255, cv2.THRESH_BINARY)
-            imgCrop = imgCv2[365:395, 490:570]
-            cropped_image_np = np.array(imgCrop)
-            gray_image = cv2.cvtColor(cropped_image_np, cv2.COLOR_BGR2GRAY)
-            blurred_image = cv2.GaussianBlur(gray_image, (1, 1), 1)
-            unsharp_mask = cv2.addWeighted(gray_image, 11, blurred_image, -10, 0)
-            blurred_image = cv2.GaussianBlur(unsharp_mask, (5, 5), 0)
-            # Read captcha
-            reader = easyocr.Reader(['en'])
-            result = reader.readtext(blurred_image)
-
-            if result:
-                text = result[0][-2]
-                # Extract only numbers from the detected text
-                numbers_only = ''.join(filter(str.isdigit, text))
-                # Ensure the extracted numbers contain exactly 4 digits
-                if len(numbers_only) == 4:
-                    capcha.send_keys(numbers_only)
-                    time.sleep(vnpt_constant.DELAY_TIME_LOAD_CAPTCHA)
-                    logging.info(f"{self.get_name()}: Detected captcha text: {numbers_only}")
-                else:
-                    logging.error(f"{self.get_name()}: Invalid captcha format.")
-            else:
-                logging.error(f"{self.get_name()}: No text detected in the captcha image.")
-
-        # Maximize the browser window to full screen
         browser = self.get_driver()
         browser.maximize_window()
         logging.info(f'{self.get_name()}: Please wait .. ({vnpt_constant.DELAY_OPEN_MAXIMUM_BROWSER}s)')
         time.sleep(vnpt_constant.DELAY_OPEN_MAXIMUM_BROWSER)
         # Open a website
-        browser.get(self.get_code_lookup())
-        logging.info(f'{self.get_name()}: Open a website: {self.get_code_lookup()}')
+        browser.get(self.get_portal())
+        logging.info(f'{self.get_name()}: Open a website: {self.get_portal()}')
         time.sleep(vnpt_constant.DELAY_TIME_LOAD_PAGE)
         logging.info(f'{self.get_name()}: Please wait .. ({vnpt_constant.DELAY_TIME_LOAD_PAGE}s)')
-
-        max_attempts = 5
-        attempt_count = 0
-        success = False  # Thêm một biến kiểm soát
-
-        while attempt_count < max_attempts:
-            # Process Captcha
-            handle_captcha_input(browser)  # Pass the browser instance
-
-            # Try to find and click the submit button
-            form_submit = WebDriverWait(browser, 10).until(
-                EC.presence_of_element_located((By.ID, "Searchform"))
-            )
-            form_submit.submit()
-
-            try:
-                link_element = WebDriverWait(browser, 10).until(
-                    EC.presence_of_element_located((By.XPATH, "//a[contains(@title, 'Tải file pdf')]"))
-                )
-                link_element.click()
-                success = True
-                break
-            except Exception as e:
-                attempt_count += 1
-                logging.warning(f"{self.get_name()}: Attempt {attempt_count} failed. Retrying...")
-
-        # Kiểm tra nếu thành công thì thực hiện các bước tiếp theo
-        if success:
-            link_element = browser.find_element(By.XPATH, "//a[contains(@title, 'Tải file pdf')]")
-            link_element.click()
-            time.sleep(vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
-            logging.info(f"{self.get_name()}: Clicked on the 'Tải file pdf' link.")
-
-            view_link = browser.find_element(By.XPATH, "//td[@class='text-center']/a[@title='Xem']")
-            view_link.click()
-            time.sleep(vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
-            logging.info(f"{self.get_name()}: Clicked on the 'Xem' link.")
-
-            download_button = browser.find_element(By.XPATH,"//button[@onclick=\"downloadZip('E11ix13GqCAToOpGHidEqI/OddSZrj5i6SlhH1+JY0s=');\"]")
-            download_button.click()
-            time.sleep(vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
-            logging.info(f"{self.get_name()}: Clicked on the 'Tải hóa đơn Zip' button.")
-            pass
+        # Enter id
+        self.enter_id(browser)
+        # Enter captcha
+        util_rpa.enter_captcha(self.get_name(), browser, By.CLASS_NAME, By.ID,
+                               vnpt_constant.CAPTCHA_IMG_BY_CLASS_TYPE,
+                               vnpt_constant.CAPTCHA_INPUT_BY_ID_TYPE,
+                               By.ID, vnpt_constant.FORM_BY_ID_TYPE, By.XPATH,
+                               vnpt_constant.ERROR_ALERT_BY_XPATH, vnpt_constant.RETRY_MAX,
+                               vnpt_constant.DELAY_TIME_SKIP, check_num=True, callback=self.enter_id,
+                               callback_args=[browser])
+        # Open view detail
+        logging.info(f'{self.get_name()}: Open view detail')
+        view_btn = browser.find_element(By.XPATH, vnpt_constant.VIEW_BTN_BY_XPATH)
+        view_btn.click()
+        logging.info(f'{self.get_name()}: Please wait .. ({vnpt_constant.DELAY_TIME_SKIP}s)')
+        time.sleep(vnpt_constant.DELAY_TIME_SKIP)
+        # Download file pdf and xml
+        logging.info(f'{self.get_name()}: Download pdf and xml')
+        download_btn = browser.find_element(By.NAME, vnpt_constant.DOWNLOAD_BTN_BY_NAME_TYPE)
+        download_btn.click()
+        logging.info(f'{self.get_name()}: Please wait .. ({vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE}s)')
+        time.sleep(vnpt_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        # Close rpa
         browser.quit()
-        logging.info(f'{self.get_name()}: Process completed successfully.')
+        logging.info(f'{self.get_name()}: Finished process download xml & pdf')
+
+
+
 
     def versions(self) -> dict:
         return vnpt_constant.VERSIONS
