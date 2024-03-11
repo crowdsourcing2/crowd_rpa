11d38ddf25e329661230cc5b81424de312abdadb
tranhoaithu <tranthihoaithu1112002@gmail.com>
2024-03-11 18:12:05 +0700

    fix: update function in core for cyberbill

--- crowd_rpa/cores/cyberbill/constant.py
+++ crowd_rpa/cores/cyberbill/constant.py
@@ -3,21 +3,21 @@ from os.path import join
 from settings import cfg
 
 
-class CyberbillConstant:
+class CyberBillConstant:
     INPUT_ID_BY_NAME ='MaSoBiMat'
+    PATH_PDF_FILE ='./RPA_TEMP/cyberbill.pdf'
     CAPTCHA_IMG_BY_XPATH ='/html/body/app-root/ng-component/ng-component/div/div/div/div/div/div[2]/div/form/div[2]/img'
     CAPTCHA_INPUT_BY_XPATH = '/html/body/app-root/ng-component/ng-component/div/div/div/div/div/div[2]/div/form/div[3]/input'
     BUTTON_SEARCH_BY_XPATH ='/html/body/app-root/ng-component/ng-component/div/div/div/div/div/div[2]/div/form/div[4]/button'
     ERROR_ALERT_BY_CLASS ='toast-message'
     DOWNLOAD_PDF_XPATH = '/html/body/app-root/ng-component/ng-component/popup-modal/div/div/div/div[3]/button[2]'
     DOWNLOAD_XML_XPATH = '/html/body/app-root/ng-component/ng-component/popup-modal/div/div/div/div[3]/button[1]'
-    URL = 'https://tracuu.cyberbill.vn/#/tracuuhoadon/tracuu'
+    URL = None
     CORE_NAME = 'CORES.CYBERBILL.RPA'
     META_DATA = {
         'URL': URL,
         'RPA_NAME': CORE_NAME,
         'DRIVER_NAME': 'chrome'
-
     }
     VERSIONS = {
         'v1': {
@@ -33,6 +33,6 @@ class CyberbillConstant:
     RETRY_MAX = 5
 
 
-cyberbill_constant = CyberbillConstant()
+cyber_bill_constant = CyberBillConstant()
 
 
--- crowd_rpa/cores/cyberbill/rpa.py
+++ crowd_rpa/cores/cyberbill/rpa.py
@@ -2,11 +2,13 @@ import logging
 import time
 from abc import ABC
 
+import PyPDF2
+import re
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 
-from crowd_rpa.cores.cyberbill.constant import cyberbill_constant
+from crowd_rpa.cores.cyberbill.constant import cyber_bill_constant
 from crowd_rpa.interfaces.rpa_interface import IRpa
 from crowd_rpa.utils.rpa_util import util_rpa
 from driver import WebDriver
@@ -23,63 +25,117 @@ class CyberbillRpa(IRpa, ABC):
         return WebDriver(tag=self.driver_name)()
 
     def get_name(self):
-        return cyberbill_constant.META_DATA['RPA_NAME']
+        return cyber_bill_constant.META_DATA['RPA_NAME']
+
+    def check_invoice(self):
+        return None
+
+    def get_portal(self):
+        urls = self.read_pdf()
+        if urls:
+            return urls[0]
+        else:
+            print("Khong tim thay duong dan")
+            return ""
 
     def get_code_lookup(self):
-        # TODO: you must be code here!
-        return cyberbill_constant.META_DATA['URL']
+        code = self.read_look_code()
+        return code
+
+    def read_look_code(self):
+        codes = []
+        try:
+            with open(cyber_bill_constant.PATH_PDF_FILE, 'rb') as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+                for page_num in range(len(pdf_reader.pages)):
+                    text = pdf_reader.pages[page_num].extract_text()
+                    start_index = text.find("Mã số tra cứu:")
+                    if start_index != -1:
+                        relevant_text = text[start_index + len("Mã số tra cứu"):].strip()
+                        code_matches = re.findall(r'[A-Za-z0-9]+', relevant_text)
+                        if code_matches:
+                            last_code = code_matches[0]
+                            if last_code.endswith('M'):
+                                last_code = last_code[:-1]
+                            codes.append(last_code)
+        except FileNotFoundError:
+            print(f"Lỗi: Không tìm thấy tệp - {cyber_bill_constant.PATH_PDF_FILE}")
+        except Exception as e:
+            print(f"Có lỗi xảy ra: {e}")
+        return codes
+
+    def read_pdf(self):
+        links = []
+        try:
+            with open(cyber_bill_constant.PATH_PDF_FILE, 'rb') as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+                for page_num in range(len(pdf_reader.pages)):
+                    text = pdf_reader.pages[page_num].extract_text()
+                    start_index = text.find("Tra cứu hóa đơn điện tử tại trang web:")
+                    if start_index != -1:
+                        relevant_text = text[start_index:]
+                        link_matches = re.findall(
+                            r'https?://(?:[a-zA-Z]|\d|[$-_@.&+]|[!*\\(),]|%[0-9a-fA-F][0-9a-fA-F])+', relevant_text)
+                        links.extend(link_matches)
+        except FileNotFoundError:
+            print(f"Lỗi: Không tìm thấy tệp - {cyber_bill_constant.PATH_PDF_FILE}")
+        except Exception as e:
+            print(f"Có lỗi xảy ra: {e}")
+        return links
+
 
     def process_download_xml_pdf(self):
         logging.info(f'{self.get_name()}: Start process download xml & pdf')
         # Maximize the browser window to full screen
         browser = self.get_driver()
         browser.maximize_window()
-        logging.info(f'{self.get_name()}: Please wait .. ({cyberbill_constant.DELAY_OPEN_MAXIMUM_BROWSER}s)')
-        time.sleep(cyberbill_constant.DELAY_OPEN_MAXIMUM_BROWSER)
+        logging.info(f'{self.get_name()}: Please wait .. ({cyber_bill_constant.DELAY_OPEN_MAXIMUM_BROWSER}s)')
+        time.sleep(cyber_bill_constant.DELAY_OPEN_MAXIMUM_BROWSER)
         # Open a website
-        browser.get(self.get_code_lookup())
-        logging.info(f'{self.get_name()}: Open a website: {self.get_code_lookup()}')
-        time.sleep(cyberbill_constant.DELAY_TIME_LOAD_PAGE)
+        browser.get(self.get_portal())
+        logging.info(f'{self.get_name()}: Open a website: {self.get_portal()}')
+        time.sleep(cyber_bill_constant.DELAY_TIME_LOAD_PAGE)
         # Enter lookup code
         logging.info(f'{self.get_name()}: Entenr lookup code')
         wait = WebDriverWait(browser, 15)  # Đợi 10 giây
-        input_id = wait.until(EC.visibility_of_element_located((By.NAME, cyberbill_constant.INPUT_ID_BY_NAME)))
-        input_id.send_keys("2E2EYBV8S3AG")
+        input_id = wait.until(EC.visibility_of_element_located((By.NAME, cyber_bill_constant.INPUT_ID_BY_NAME)))
+        input_id.send_keys(self.get_code_lookup())
         # Enter captcha
         util_rpa.enter_captcha(self.get_name(), browser, By.XPATH, By.XPATH,
-                                   cyberbill_constant.CAPTCHA_IMG_BY_XPATH,
-                                   cyberbill_constant.CAPTCHA_INPUT_BY_XPATH, By.XPATH,
-                                   cyberbill_constant.BUTTON_SEARCH_BY_XPATH, By.CLASS_NAME,
-                                   cyberbill_constant.ERROR_ALERT_BY_CLASS, cyberbill_constant.RETRY_MAX, cyberbill_constant.DELAY_TIME_SKIP, check_num=True)
-        time.sleep(cyberbill_constant.DELAY_TIME_SKIP)
+                               cyber_bill_constant.CAPTCHA_IMG_BY_XPATH,
+                               cyber_bill_constant.CAPTCHA_INPUT_BY_XPATH, By.XPATH,
+                               cyber_bill_constant.BUTTON_SEARCH_BY_XPATH, By.CLASS_NAME,
+                               cyber_bill_constant.ERROR_ALERT_BY_CLASS, cyber_bill_constant.RETRY_MAX,
+                               cyber_bill_constant.DELAY_TIME_SKIP, check_num=True)
+        time.sleep(cyber_bill_constant.DELAY_TIME_SKIP)
         logging.info(f'{self.get_name()}: Download PDF')
-        download_pdf = browser.find_element(By.XPATH, cyberbill_constant.DOWNLOAD_PDF_XPATH)
+        download_pdf = browser.find_element(By.XPATH, cyber_bill_constant.DOWNLOAD_PDF_XPATH)
         download_pdf.click()
         download_pdf.click()
         download_pdf.click()
         download_pdf.click()
-        logging.info(f'{self.get_name()}: Please wait .. ({cyberbill_constant.DELAY_TIME_SKIP}s)')
-        time.sleep(cyberbill_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        logging.info(f'{self.get_name()}: Please wait .. ({cyber_bill_constant.DELAY_TIME_SKIP}s)')
+        time.sleep(cyber_bill_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
         logging.info(f'{self.get_name()}: Download XML')
-        download_xml = browser.find_element(By.XPATH, cyberbill_constant.DOWNLOAD_XML_XPATH)
+        download_xml = browser.find_element(By.XPATH, cyber_bill_constant.DOWNLOAD_XML_XPATH)
         download_xml.click()
-        logging.info(f'{self.get_name()}: Please wait .. ({cyberbill_constant.DELAY_TIME_SKIP}s)')
+        logging.info(f'{self.get_name()}: Please wait .. ({cyber_bill_constant.DELAY_TIME_SKIP}s)')
 
-        time.sleep(cyberbill_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        time.sleep(cyber_bill_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
         browser.quit()
         logging.info(f'{self.get_name()}: Finished process download xml & pdf')
 
     def versions(self) -> dict:
-        return cyberbill_constant.VERSIONS
+        return cyber_bill_constant.VERSIONS
 
     def get_latest_version(self) -> dict:
         return {
-            'version': cyberbill_constant.LATEST_VERSION,
-            'info': cyberbill_constant.VERSIONS[cyberbill_constant.LATEST_VERSION]
+            'version': cyber_bill_constant.LATEST_VERSION,
+            'info': cyber_bill_constant.VERSIONS[cyber_bill_constant.LATEST_VERSION]
         }
 
 
 if __name__ == '__main__':
-    cyberbill_rpa_ins = CyberbillRpa(cyberbill_constant.META_DATA)
+    cyberbill_rpa_ins = CyberbillRpa(cyber_bill_constant.META_DATA)
     cyberbill_rpa_ins.extract_data()
     cyberbill_rpa_ins.reset()
