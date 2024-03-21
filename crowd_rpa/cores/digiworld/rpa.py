4df1d51d1727f11c7312544561754491178868f6
toandev020101 <toandev020101@gmail.com>
2024-03-21 16:38:07 +0700

    fix: time skip and extra zip to pdf, xml

--- crowd_rpa/cores/digiworld/rpa.py
+++ crowd_rpa/cores/digiworld/rpa.py
@@ -75,9 +75,9 @@ class DigiWorldRpa(IRpa, ABC):
         logging.info(f'{self.get_name()}: Please wait .. ({digi_world_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE}s)')
         time.sleep(digi_world_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
         # extra zip
-        directory_path = f"{storage_pth}/{self.get_name()}/{self.get_name().lower()}"
+        directory_path = f"{storage_pth}/{self.get_name().lower()}/test"
         util_rpa.extract_zip_files_and_keep_specific_files(directory_path)
-        time.sleep(digi_world_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        time.sleep(digi_world_constant.DELAY_TIME_SKIP)
 
         # Close rpa
         browser.close()
