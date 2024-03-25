2890b33e7fd3662ea1580d5114e6c2f26dd10884
toandev020101 <toandev020101@gmail.com>
2024-03-25 22:07:37 +0700

    fix: portal rosysoft

--- crowd_rpa/cores/rosysoft/rpa.py
+++ crowd_rpa/cores/rosysoft/rpa.py
@@ -1,8 +1,13 @@
-import logging
+import os
 import time
+import logging
 from abc import ABC
+from pathlib import Path
 
-from crowd_rpa.utils.rpa_util import util_rpa
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+from crowd_rpa.settings import cfg
 from crowd_rpa.driver import WebDriver
 from selenium.webdriver.common.by import By
 from crowd_rpa.interfaces.rpa_interface import IRpa
@@ -13,57 +18,31 @@ class RosySoftRpa(IRpa, ABC):
     def __init__(self, meta_data):
         super().__init__(meta_data)
 
-    def get_portal(self):
-        url = ""
-        path = rosysoft_constant.FILE_PATH
-        if path.endswith(".pdf"):
-            url = util_rpa.read_pdf(path, rosysoft_constant.URL_KEYWORD,
-                                    rosysoft_constant.CODE_BILL_KEYWORD)
-        elif path.endswith(".xml"):
-            url = util_rpa.read_xml(path, rosysoft_constant.URL_KEYWORD)
-        print(url)
-        return url
+    def extract_data(self, portal, lookup_code, storage_pth, filename, company_code=None):
+        return self.process_download_xml_pdf(portal, lookup_code, storage_pth, filename, company_code)
 
-    def get_code_lookup(self):
-        code_bill = ""
-        path = rosysoft_constant.FILE_PATH
-        if path.endswith(".pdf"):
-            code_bill = util_rpa.read_pdf(path, rosysoft_constant.CODE_BILL_KEYWORD,
-                                          rosysoft_constant.LAST_KEYWORD)
-        elif path.endswith(".xml"):
-            code_bill = util_rpa.read_xml(path, rosysoft_constant.CODE_BILL_KEYWORD)
-        return code_bill
-
-    def check_invoice(self):
-        taxt_code = ""
-        path = rosysoft_constant.FILE_PATH
-        if path.endswith(".pdf"):
-            taxt_code = util_rpa.read_pdf(path, rosysoft_constant.TAX_CODE_KEYWORD,
-                                          rosysoft_constant.LAST_TAXT_KEYWORD)
-        elif path.endswith(".xml"):
-            taxt_code = util_rpa.read_xml(path, rosysoft_constant.TAX_CODE_KEYWORD)
-        taxt_code = taxt_code.replace(' ', '')
-        return taxt_code
-
-    def extract_data(self):
-        self.process_download_xml_pdf()
-
-    def get_driver(self):
-        return WebDriver(tag=self.driver_name)()
+    def get_driver(self, download_directory=None, more_option=False):
+        return WebDriver(tag=self.driver_name, download_directory=download_directory, more_option=more_option)()
 
     def get_name(self):
         return rosysoft_constant.META_DATA['RPA_NAME']
 
-    def process_download_xml_pdf(self):
+    def process_download_xml_pdf(self, portal, lookup_code, storage_pth, filename, company_code):
         logging.info(f'{self.get_name()}: Start process download xml & pdf.')
 
-        browser = self.get_driver()
+        portal_pth = os.path.join(storage_pth, rosysoft_constant.CORE_NAME.lower())
+        if not Path(portal_pth).is_dir():
+            os.mkdir(portal_pth)
+        save_pth = os.path.join(portal_pth, filename)
+        if not Path(save_pth):
+            os.mkdir(save_pth)
+        browser = self.get_driver(save_pth, True)
         browser.maximize_window()
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_OPEN_MAXIMUM_BROWSER}s)')
         time.sleep(rosysoft_constant.DELAY_OPEN_MAXIMUM_BROWSER)
 
-        browser.get(self.get_portal())
-        logging.info(f'{self.get_name()}: Open a website: {self.get_portal()}')
+        browser.get(portal)
+        logging.info(f'{self.get_name()}: Open a website: {portal}')
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_TIME_LOAD_PAGE}s)')
         time.sleep(rosysoft_constant.DELAY_TIME_LOAD_PAGE)
 
@@ -72,14 +51,14 @@ class RosySoftRpa(IRpa, ABC):
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_TIME_SKIP}s)')
         time.sleep(rosysoft_constant.DELAY_TIME_SKIP)
 
-        logging.info(f'{self.get_name()}: Enter data code lookup: {self.check_invoice()} into the from.')
+        logging.info(f'{self.get_name()}: Enter data code lookup: {company_code} into the form.')
         text_box_tax_code = browser.find_element(By.ID, rosysoft_constant.TEXT_TAXCODE_BY_ID_TYPE)
-        text_box_tax_code.send_keys(self.check_invoice())
+        text_box_tax_code.send_keys(company_code)
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_TIME_SKIP}s)')
         time.sleep(rosysoft_constant.DELAY_TIME_SKIP)
 
         text_box_search_code = browser.find_element(By.ID, rosysoft_constant.TEXT_SEARCH_CODE_BY_ID_TYPE)
-        text_box_search_code.send_keys(self.get_code_lookup())
+        text_box_search_code.send_keys(lookup_code)
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_TIME_SKIP}s)')
         time.sleep(rosysoft_constant.DELAY_TIME_SKIP)
 
@@ -89,20 +68,25 @@ class RosySoftRpa(IRpa, ABC):
         submit_btn.click()
         time.sleep(rosysoft_constant.DELAY_TIME_SKIP)
 
-        logging.info(f'{self.get_name()}: URL redirection: {rosysoft_constant.URL_N}.')
-        browser.get(rosysoft_constant.URL_N)
+        windows = browser.window_handles
+        logging.info(f'{self.get_name()}: Switch to new tab: {windows[-1]}.')
+        browser.switch_to.window(windows[-1])
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_TIME_LOAD_PAGE}s)')
         time.sleep(rosysoft_constant.DELAY_TIME_LOAD_PAGE)
 
         logging.info(f'{self.get_name()}: Download PDF')
         pdf_down = browser.find_element(By.XPATH, rosysoft_constant.DOWNLOAD_PDF_XPATH)
-        pdf_down.click()
+        wait = WebDriverWait(browser, rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        wait.until(ec.presence_of_element_located((By.XPATH, rosysoft_constant.DOWNLOAD_PDF_XPATH)))
+        browser.execute_script("arguments[0].click();", pdf_down)
         logging.info(f'{self.get_name()}:Please wait .. ({rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE}s)')
         time.sleep(rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
 
         logging.info(f'{self.get_name()}: Download XML')
         xml_down = browser.find_element(By.XPATH, rosysoft_constant.DOWNLOAD_XML_XPATH)
-        xml_down.click()
+        wait = WebDriverWait(browser, rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
+        wait.until(ec.presence_of_element_located((By.XPATH, rosysoft_constant.DOWNLOAD_XML_XPATH)))
+        browser.execute_script("arguments[0].click();", xml_down)
         logging.info(f'{self.get_name()}: Please wait .. ({rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE}s)')
         time.sleep(rosysoft_constant.DELAY_CLICK_DOWNLOAD_EVERY_FILE)
 
@@ -121,7 +105,10 @@ class RosySoftRpa(IRpa, ABC):
 
 rosy_soft_ins = RosySoftRpa(rosysoft_constant.META_DATA)
 
-
 if __name__ == '__main__':
-    rosy_soft_ins.extract_data()
+    rosy_soft_ins.extract_data("https://eInv.rosySoft.vn:8386",
+                               "A01TXL000630801",
+                               cfg.TEST_ROOT_PTH,
+                               'test',
+                               company_code='0302035520')
     rosy_soft_ins.reset()
